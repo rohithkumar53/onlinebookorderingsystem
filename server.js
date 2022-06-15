672ce8c3748c6492b4c28890a96a6f69f2e3d23a
Rohith <rohithkumar.pampari@gmail.com>
2022-06-15 21:38:15 +0100

    changed package json and server last time

--- server.js
+++ server.js
@@ -17,11 +17,7 @@ app.use('/api/users', require("./routes/userRoute"));
 app.use('/api/orders', require("./routes/ordersRoute"));
 
 if(process.env.NODE_ENV === "production"){
-    app.use('/', express.static('frontend/build'));
-    
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend/build/index.html'))
-    })
+    app.use(express.static('frontend/build'));
 }
 
 //port listening
