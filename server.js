91ebc77ef0457736bc31d01444d4dc9670abe861
Rohith <rohithkumar.pampari@gmail.com>
2022-06-15 19:29:57 +0100

    changed package.json and server.js

--- server.js
+++ server.js
@@ -17,9 +17,10 @@ app.use('/api/users', require("./routes/userRoute"));
 app.use('/api/orders', require("./routes/ordersRoute"));
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../build")));
+    app.use('/', express.static('frontend/build'));
+    
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../build", "index.html"))
+        res.sendFile(path.resolve(__dirname, 'frontend/build/index.html'))
     })
 }
 
